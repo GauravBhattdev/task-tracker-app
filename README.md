6f40c08bc1b6cdb081d6aa668950aad3f3fba959
GauravBhatt <bhatt.gourav92@gmail.com>
2026-05-22 18:30:42 +0530

    Initial Project Setup

---
+++ README.md
@@ -0,0 +1,35 @@
+# Task Tracker App
+
+# Description
+A responsive  task management web application built  using HTML , CSS and JavaScript.
+
+# Features 
+
+### Add Task 
+Users can dynamically add tasks  to the tracker.
+
+
+### Delete Task
+Tasks can be removed individually.
+
+### Mark as Completed
+Completed tasks are visually differentiated using strike-through styling.
+
+### Local Storage
+Tasks are stored in browser local storage and persist after refresh.
+
+### Dark Mode
+Users can switch between light and dark themes.
+
+### Search Feature
+Tasks can be searched in real-time.
+
+### Task Priority
+Users can assign Low, Medium, or High priority labels.
+
+### Toast Notifications
+Notifications appear after actions like add, delete, or edit.
+
+### Responsive Design
+The layout adapts properly for mobile and desktop screens.
+
---
+++ script.js
@@ -0,0 +1,167 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+const themeToggle = document.getElementById("themeToggle");
+const searchInput = document.getElementById("searchInput");
+const completedCount = document.getElementById("completedCount");
+const pendingCount = document.getElementById("pendingCount");
+const priorityInput = document.getElementById("priority");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+renderTasks();
+
+addTaskBtn.addEventListener("click", addTask);
+
+searchInput.addEventListener("input", renderTasks);
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
+
+function addTask() {
+
+    const text = taskInput.value.trim();
+
+    if (text === "") {
+        showToast("Task cannot be empty");
+        return;
+    }
+
+    const task = {
+        id: Date.now(),
+        text: text,
+        completed: false,
+        priority: priorityInput.value
+    };
+
+    tasks.push(task);
+
+    saveTasks();
+
+    taskInput.value = "";
+
+    renderTasks();
+
+    showToast("Task Added");
+}
+
+function renderTasks() {
+
+    taskList.innerHTML = "";
+
+    const searchValue = searchInput.value.toLowerCase();
+
+    const filteredTasks = tasks.filter(task =>
+        task.text.toLowerCase().includes(searchValue)
+    );
+
+    filteredTasks.forEach(task => {
+
+        const li = document.createElement("li");
+
+        li.className = `task ${task.completed ? "completed" : ""}`;
+
+        li.innerHTML = `
+            <div>
+                <span>${task.text}</span>
+                <span class="priority ${task.priority.toLowerCase()}">
+                    ${task.priority}
+                </span>
+            </div>
+
+            <div class="task-buttons">
+
+                <button onclick="toggleTask(${task.id})">
+                  '\u2714'
+                </button>
+
+                <button onclick="editTask(${task.id})">
+                   "\u270F"
+                </button>
+
+                <button onclick="deleteTask(${task.id})">
+                    "\u2715"
+                </button>
+
+            </div>
+        `;
+
+        taskList.appendChild(li);
+    });
+
+    updateCounters();
+}
+
+function toggleTask(id) {
+
+    tasks = tasks.map(task => {
+
+        if (task.id === id) {
+            task.completed = !task.completed;
+        }
+
+        return task;
+    });
+
+    saveTasks();
+
+    renderTasks();
+}
+
+function deleteTask(id) {
+
+    tasks = tasks.filter(task => task.id !== id);
+
+    saveTasks();
+
+    renderTasks();
+
+    showToast("Task Deleted");
+}
+
+function editTask(id) {
+
+    const task = tasks.find(task => task.id === id);
+
+    const updatedText = prompt("Edit Task", task.text);
+
+    if (updatedText !== null) {
+        task.text = updatedText;
+
+        saveTasks();
+
+        renderTasks();
+
+        showToast("Task Updated");
+    }
+}
+
+function updateCounters() {
+
+    const completedTasks = tasks.filter(task => task.completed).length;
+
+    const pendingTasks = tasks.length - completedTasks;
+
+    completedCount.textContent = completedTasks;
+
+    pendingCount.textContent = pendingTasks;
+}
+
+function saveTasks() {
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function showToast(message) {
+
+    const toast = document.getElementById("toast");
+
+    toast.innerText = message;
+
+    toast.style.display = "block";
+
+    setTimeout(() => {
+        toast.style.display = "none";
+    }, 2000);
+}
